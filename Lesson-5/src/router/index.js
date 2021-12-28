078a035e05196c86db3dbc6d48659ebfc4f651b2
diana-kot <57452102+diana-kot@users.noreply.github.com>
2021-12-28 19:46:09 +0400

    Урок 5. Устанавливаем и настраиваем vue-router

---
+++ Lesson-5/src/router/index.js
@@ -0,0 +1,72 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import Dashboard from '../views/Dashboard.vue';
+import About from '../views/About.vue';
+import NotFound from '../views/NotFound.vue';
+import AddNewPayments from '../views/AddNewPayments.vue';
+
+Vue.use(VueRouter)
+
+
+const routes = [{
+        path: '/dashboard/:page',
+        name: "dashboard",
+        component: Dashboard
+    },
+    {
+        path: '/dashboard',
+        name: "dashboard",
+        component: Dashboard
+    },
+    {
+        path: '/about*',
+        name: "about",
+        component: About
+    },
+    {
+        path: '/notFound',
+        name: "notFound",
+        component: NotFound
+    },
+    {
+        path: '/add/payment/:category',
+        name: 'AddPaymentFromUrl',
+        component: AddNewPayments
+    },
+    {
+        path: '*',
+        component: NotFound
+    },
+
+];
+const router = new VueRouter({
+    mode: 'history',
+    routes,
+});
+
+const userAuth = true;
+router.beforeEach((to, from, next) => {
+    if (to.name !== "notfound" && !userAuth) {
+        next({
+            name: "notfound",
+        });
+    } else {
+        next();
+    }
+});
+
+
+const getTitleByRouters = (routeName) => {
+    return {
+        'dashboard': 'страница затрат',
+        'about': 'страница о сервисе',
+        'notFound': 'страница не найдена',
+    } [routeName]
+};
+
+router.afterEach((to) => {
+    document.title = getTitleByRouters(to.name)
+})
+
+export default router;
---
+++ Lesson-5/src/views/AddNewPayments.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="wrapper">
+        <div class="paments">
+            <span>Добавить статью затрат:</span>
+            <router-link
+                :to="{
+                    name: 'AddPaymentForm',
+                    params: { category: 'Food' },
+                    query: { value: '200' },
+                }"
+            >Food 200</router-link>
+            <router-link
+                :to="{
+                    name: 'AddPaymentForm',
+                    params: { category: 'Transport' },
+                    query: { value: '50' },
+                }"
+            >Transport 50</router-link>
+            <router-link
+                :to="{
+                    name: 'AddPaymentForm',
+                    params: { category: 'Entertainment' },
+                    query: { value: '50' },
+                }"
+            >Entertainment 2000</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AddNewPayments',
+};
+
+</script>
+
+<style>
+.paments {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    justify-content: space-between;
+    gap: 30px;
+    margin-bottom: 20px;
+}
+</style>
