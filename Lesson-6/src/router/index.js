b5e5dc164a904d3979fe138295f72fe558fd0fba
diana-kot <57452102+diana-kot@users.noreply.github.com>
2022-01-06 14:48:14 +0400

    .

---
+++ Lesson-6/src/router/index.js
@@ -0,0 +1,78 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+
+
+
+
+Vue.use(VueRouter)
+
+
+const routes = [{
+        path: '/dashboard/:page',
+        name: "dashboard",
+        component: () =>
+            import( /* webpackChunkName: "Dashboard" */ "../views/Dashboard.vue"),
+
+    },
+    {
+        path: '/dashboard',
+        name: "dashboard",
+        component: () =>
+            import( /* webpackChunkName: "Dashboard" */ "../views/Dashboard.vue"),
+    },
+    {
+        path: '/about*',
+        name: "about",
+        component: () =>
+            import( /* webpackChunkName: "About" */ "../views/About.vue"),
+    },
+    {
+        path: '/notFound',
+        name: "notFound",
+        component: () =>
+            import( /* webpackChunkName: "NotFound" */ "../views/NotFound.vue"),
+    },
+    {
+        path: '/add/payment/:category',
+        name: 'AddNewPayments',
+        component: () =>
+            import( /* webpackChunkName: "AddPaymentForm" */ "../components/AddPaymentForm.vue"),
+    },
+    {
+        path: '*',
+        component: () =>
+            import( /* webpackChunkName: "NotFound" */ "../views/NotFound.vue"),
+    },
+
+];
+const router = new VueRouter({
+    mode: 'history',
+    routes,
+});
+
+const userAuth = true;
+router.beforeEach((to, from, next) => {
+    if (to.name !== "notfound" && !userAuth) {
+        next({
+            name: "notfound",
+        });
+    } else {
+        next();
+    }
+});
+
+
+const getTitleByRouters = (routeName) => {
+    return {
+        'dashboard': 'страница затрат',
+        'about': 'страница о сервисе',
+        'notFound': 'страница не найдена',
+    } [routeName]
+};
+
+router.afterEach((to) => {
+    document.title = getTitleByRouters(to.name)
+})
+
+export default router;
---
+++ Lesson-6/src/store/index.js
@@ -0,0 +1,88 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        paymentList: [],
+        categoryList: [],
+        paymentListIds: [],
+    },
+    mutations: {
+        setPaymentsListData(state, payload) {
+            const newUnitIds = payload.filter((item) => {
+                return state.paymentListIds.indexOf(item.id) < 0
+            })
+            const uniqIds = newUnitIds.map(obj => obj.id)
+            state.paymentList.push(...newUnitIds);
+            state.paymentListIds.push(...uniqIds);
+        },
+        addPaymentListData(state, data) {
+            state.paymentList.push(data)
+
+        },
+        getPaymentListFirstElement(state, payload) {
+            Vue.set(state.paymentList, 0, payload)
+        },
+        setCategoryData(state, paylaod) {
+            state.categoryList = paylaod;
+        },
+        addCategoryToList(state, payload) {
+            state.categoryList.push(payload)
+        },
+        delItemFromPaymentList (state, itemId) {
+            const item = state.paymentList.find(el=> el.id === itemId)
+            const indexItem = state.paymentList.indexOf(item)
+            state.paymentList.splice(indexItem, 1)
+        },
+        editDataInPaymentList(state, payload) {
+            state.paymentList = state.paymentList.map(item => {
+                if (item.id === payload.id) {
+                  return Object.assign({}, item, payload)
+                }
+
+                return item
+            })
+        }
+    },
+    getters: {
+        getPaymentListFullValuePrise: state => {
+            return state.paymentList.reduce((pre, cur) => pre + cur.value, 0)
+        },
+        getPaymentList: state => state.paymentList,
+        getCategoryList: state => state.categoryList,
+        getPaymentsItem: (state) => (index) => {
+            return state.paymentsList[index -1]
+          },
+
+    },
+    actions: {
+        fetchData({ commit }) {
+            return new Promise((resolve) => {
+              setTimeout(() => {
+                const items = [];
+                for (let i = 1; i < 12; i++) {
+                  items.push({
+                    date: "20.12.2021",
+                    category: "Education",
+                    value: i,
+                    id: i,
+                  });
+                }
+                resolve(items);
+              }, 2000);
+            }).then((res) => commit("setPaymentsListData", res));
+          },
+
+        fetchCategory ({commit}) {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    const items = ["Sport", "Education", "Internet", "Food", "Transport"];
+                    resolve(items);
+                }, 1000);
+            }).then((res) => commit("setCategoryData", res));
+        },
+
+    },
+});
