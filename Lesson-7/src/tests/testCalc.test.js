e8a3ae8e764566cc583ae42ed3f7f6220d7fc331
diana-kot <57452102+diana-kot@users.noreply.github.com>
2022-01-07 00:01:34 +0400

    7.01.22

---
+++ Lesson-7/src/tests/testCalc.test.js
@@ -0,0 +1,13 @@
+import {mount} from '@vue/test-utils'
+import Calculator from '..components/Calculator.vue'
+import { iterator } from 'core-js/fn/symbol'
+
+describe('Calculator', ()=>{
+    it('test first operand input value', ()=>{
+
+    })
+
+    it('test second operand input value', ()=>{
+        
+    })
+})
