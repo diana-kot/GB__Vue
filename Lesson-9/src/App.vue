9dae8e8c029cceed1464def1cb2e72306d03f6bc
diana-kot <57452102+diana-kot@users.noreply.github.com>
2022-01-19 00:02:56 +0400

    дабавлен date picker

---
+++ Lesson-9/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-app-bar app color="teal">
+      <v-btn :plain="true" dark :ripple="false" to="/costs">Costs</v-btn>
+      <v-btn plain dark :ripple="false" to="/about">About</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style lang="scss">
+
+</style>
+
---
+++ Lesson-9/src/components/AddPaymentForm.vue
@@ -0,0 +1,174 @@
+<template>
+  <v-card class="text-left pa-8">
+    <v-container>
+    <v-row>
+     
+        <v-menu
+          ref="menu1"
+          v-model="menu1"
+          :close-on-content-click="false"
+          transition="scale-transition"
+          offset-y
+          max-width="290px"
+          min-width="auto"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="dateFormatted"
+              label="Date"
+              hint="DD/MM/YY"
+              persistent-hint
+              prepend-icon="mdi-calendar"
+              v-bind="attrs"
+              @blur="date = parseDate(dateFormatted)"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker
+            v-model="date"
+            no-title
+            @input="menu1 = false"
+          ></v-date-picker>
+        </v-menu>
+    </v-row>
+  </v-container>
+    <v-text-field label="Payment amount" v-model="value"></v-text-field>
+    <v-select
+      :items="getCategoryList"
+      label="Category"
+      v-model="category"
+    ></v-select>
+    <v-btn class="save_btn" @click="onSave()"
+      >SAVE <v-icon>mdi-plus</v-icon></v-btn
+    >
+  </v-card>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "AddPaymentForm",
+  props: {
+    editedElem: Object,
+  },
+  
+  data: vm => ( {
+    
+      date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+      dateFormatted: vm.formatDate((new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10)),
+      menu1: false,
+      isDisabled: false,
+      category: "",
+      value: "",
+      new_category: "",
+      
+    
+  }),
+  computed: {
+    ...mapGetters(["getCategoryList"]),
+    getCurrentDate() {
+      const today = new Date();
+      const d = today.getDate();
+      const m = today.getMonth();
+      const y = today.getFullYear();
+      return `${d}.${m}.${y}`;
+    },
+    computedDateFormatted () {
+        return this.formatDate(this.date)
+        
+      },
+  },
+  methods: {
+    ...mapMutations({
+      addCategory: "addNewCategory",
+    }),
+    ...mapActions(["fetchCategoryList"]),
+    addToCategoryList() {
+      this.addCategory(this.new_category);
+    },
+    onSave() {
+      if (this.editedElem) {
+        const data = {
+          id: this.editedElem.id,
+          category: this.category,
+          value: Number(this.value),
+          
+          
+        };
+        this.$store.dispatch("upgradeData", data);
+        this.$emit("closeAddPayment");
+        this.category = "";
+        this.value = "";
+        
+      } else {
+        const data = {
+          id: 0,
+          category: this.category,
+          value: Number(this.value),
+          date: this.formatDate(this.date) || this.formatDate(this.getCurrentDate),
+        };
+        if (data.category !== '' && data.value !== ''){
+          this.$emit("addNewPayment", data);
+        }
+        
+      }
+      this.data = ''
+      this.category = "";
+      this.value = "";
+      this.date = "";
+    },
+    changeElem() {
+      if (this.editedElem) {
+        this.isDisabled = true;
+        this.category = this.editedElem.category;
+        this.value = this.editedElem.value;
+      } else {
+        this.isDisabled = false;
+        this.category = "";
+        this.value = "";
+        this.date = "";
+      }
+    },
+    formatDate (date) {
+        if (!date) return null
+        const options = {}
+        return new Intl.DateTimeFormat('ru-Ru', options).format(new Date(date))
+      },
+
+      parseDate (date) {
+        if (!date) return null
+
+        const [ day, month, year] = date.split('.')
+        return `${day.padStart(2, '0')}.${month.padStart(2, '0')}.${year}`
+        
+      },
+    
+  },
+  mounted() {
+    if (!this.getCategoryList?.length) {
+      this.fetchCategoryList();
+    }
+    this.changeElem();
+    if (this.$route.params.category) {
+      this.category = this.$route.params.category;
+    }
+    if (this.$route.query.value) {
+      this.value = this.$route.query.value;
+    }
+  },
+  watch: {
+    editedElem() {
+      this.changeElem();
+    },
+    date () {
+        this.dateFormatted = this.formatDate(this.date)
+        
+      },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
